99bfb2fd24fa2d16982e8f8dec04440f768cab28
Tim Sutton <tim@kartoza.com>
2017-08-18 21:59:41 +0200

    First version  - gps extract not working yet

--- TakeAPhoto/ViewController.swift
+++ TakeAPhoto/ViewController.swift
@@ -7,9 +7,69 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UINavigationControllerDelegate,
+                      UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageDetails: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let imageController = UIImagePickerController()
+        imageController.delegate = self
+        imageController.sourceType = UIImagePickerControllerSourceType.camera
+        imageController.allowsEditing = false
+        self.present(imageController, animated: true)
+        {
+            // after it is complete
+        }
+    }
+    
+    @IBAction func savePhoto(_ sender: AnyObject) {
+        // logic from https://stackoverflow.com/q/45277034
+        let imageData = UIImagePNGRepresentation(image.image!)
+        let compresedImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compresedImage!, nil, nil, nil)
+        
+        let alert = UIAlertController(title: "Saved", message: "Your image has been saved", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            image.image = selectedImage
+        } else {
+            // handle error
+        }
+
+        if let URL = info[UIImagePickerControllerMediaURL] as? URL {
+            let opts = PHFetchOptions()
+            opts.fetchLimit = 1
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [URL], options: opts)
+            let asset = assets[0]
+            if asset == nil {
+                imageDetails.text = "No image captured."
+            }
+            //location: CLLocation = asset.location
+            //coordinate: CLLocationCoordinate2D = location.coordinate
+            //latitude = asset.location.coordinate.latitude
+            //longitude = asset.location.coordinate.longitude()
+            //imageDetails.text =  "Position: \(longitude), \(latitude)"
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
