4b42dc9ce6325e44adc6938c56cb1a70c3aab465
Tim Sutton <tim@kartoza.com>
2017-10-01 05:57:00 +0700

    Adding new controllers for data capture work flow
    Signed-off-by: Tim Sutton <tim@kartoza.com>

--- TakeAPhoto/ImageCaptureController.swift
+++ TakeAPhoto/ImageCaptureController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImageCaptureController.swift
 //  TakeAPhoto
 //
 //  Created by Tim Sutton on 2017/08/16.
@@ -9,26 +9,25 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController,
-                      UINavigationControllerDelegate,
-                      UIImagePickerControllerDelegate,
-                      CLLocationManagerDelegate {
+class ImageCaptureController:
+    UIViewController,
+    UINavigationControllerDelegate,
+    UIImagePickerControllerDelegate,
+    CLLocationManagerDelegate {
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var captureImageButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
-    var imageDetails: String!
-    let manager = CLLocationManager() // for getting GPS coords
-    var currentLocation: CLLocationCoordinate2D = CLLocationCoordinate2D()// for storing last coord
-    
+    let locationManager = CLLocationManager() // for getting GPS coords
+    var currentLocationCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()// for storing last coord
     
     @IBAction func buttonPressed(_ sender: Any) {
-        let imageController = UIImagePickerController()
-        imageController.delegate = self
-        imageController.sourceType = UIImagePickerControllerSourceType.camera
-        imageController.allowsEditing = false
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.camera
+        imagePickerController.allowsEditing = false
         disableSaveButton()
-        self.present(imageController, animated: true)
+        self.present(imagePickerController, animated: true)
         {
             // after it is complete
         }
@@ -36,8 +35,8 @@ class ViewController: UIViewController,
     
     @IBAction func savePhoto(_ sender: AnyObject) {
         // logic from https://stackoverflow.com/q/45277034
-        let imageData = UIImagePNGRepresentation(image.image!)
-        let compresedImage = UIImage(data: imageData!)
+        let imagePNGRepresentation = UIImagePNGRepresentation(image.image!)
+        let compresedImage = UIImage(data: imagePNGRepresentation!)
         UIImageWriteToSavedPhotosAlbum(compresedImage!, nil, nil, nil)
         
         let alert = UIAlertController(title: "Saved", message: "Your image has been saved", preferredStyle: .alert)
@@ -55,21 +54,9 @@ class ViewController: UIViewController,
             // handle error
             disableSaveButton()
         }
-
-        // I searched long and hard to get the location direct from the image.
-        // You can get it form a photo roll image but not one direclty captured
-        // by using the camera. See https://stackoverflow.com/a/42888731 
-        // In particular read all the comments there carefully as there are a 
-        // lot of red herrings where the question is answered based on useing
-        // the photo roll.  Because of all these issues, I opted to get the 
-        // location direct from the GPS rather than the image rather.
-        
-        self.imageDetails = "\(currentLocation.longitude) : \(currentLocation.latitude)"
-        
         self.dismiss(animated: true, completion: nil)
         
     }
-    
 
     func enableSaveButton() {
         self.saveButton.isEnabled = true
@@ -80,22 +67,15 @@ class ViewController: UIViewController,
         self.saveButton.isEnabled = false
         self.saveButton.backgroundColor = .gray    }
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         disableSaveButton()
         // configuration for things relating to location manager.
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
-    }
-    
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     override func didReceiveMemoryWarning() {
@@ -103,20 +83,27 @@ class ViewController: UIViewController,
         // Dispose of any resources that can be recreated.
     }
 
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation])
     {
+        // I searched long and hard to get the location direct from the image.
+        // You can get it from a photo roll image but not one direclty captured
+        // by using the camera. See https://stackoverflow.com/a/42888731
+        // In particular read all the comments there carefully as there are a
+        // lot of red herrings where the question is answered based on useing
+        // the photo roll.  Because of all these issues, I opted to get the
+        // location direct from the GPS rather than the image
         let location = locations[0]
-        self.currentLocation = location.coordinate
+        self.currentLocationCoordinate = location.coordinate
     }
-
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "ImageDetails") {
             // first downcast the destination controller to an ImageDetails controller
             // so that we can access its members
             let controller = segue.destination as! ImageDetailsViewController
-            controller.location = self.currentLocation
+            controller.location = self.currentLocationCoordinate
         }
         //segue.destination.imageDetailsLabel.text = self.imageDetails
     }
