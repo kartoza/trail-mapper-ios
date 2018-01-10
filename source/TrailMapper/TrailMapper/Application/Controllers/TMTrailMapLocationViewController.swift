1cadb8315cca123d1933d1da5bd8e16d84452b82
dhananjaynetwin <dhananjay.pawar@netwinindia.biz>
2018-01-10 14:08:11 +0530

    Conflicts resolved for project file(.pbxproj)

--- source/TrailMapper/TrailMapper/Application/Controllers/TMTrailMapLocationViewController.swift
+++ source/TrailMapper/TrailMapper/Application/Controllers/TMTrailMapLocationViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class TMTrailMapLocationVControllerViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate{
+class TMTrailMapLocationViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate{
 
     //MARK:- IBOutlets
     @IBOutlet weak var trailLocationMapView: MKMapView!
