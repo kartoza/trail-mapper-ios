65844ec6dfce958100a873f3c9793addb57e9ba0
Tim Sutton <tim@kartoza.com>
2018-01-11 11:46:53 +0200

    finished migrating data to sqlite scheme

--- docs/postgres_schema.sql
+++ docs/postgres_schema.sql
@@ -256,7 +256,7 @@ ORDER BY
 
 ))as trail_section_list from trails.trail;
 
-COMMENT ON VIEW trails.vw_trail_with_sections IS 'This view lists each trail followed by a list of the associated trail_sections. Only guids are shown.';
+COMMENT ON VIEW trails.vw_trail_with_sections IS 'This view lists each trail followed by a list of the associated trail_sections. Only guids are shown from the trail table.';
 
 
 ALTER TABLE vw_trail_with_sections OWNER TO timlinux;
@@ -291,7 +291,28 @@ COMMENT ON VIEW trails.vw_trail_sqlite_export IS 'This view generates insert sta
 update trails.trail_section set image='images/Marloth-with-WHS-logo-1.jpg';
 create view trails.vw_trail_sections_sqlite_export as select 'insert into trail_section (name, notes, image, grade_guid, guid, geom) values ("' ||
 name || '","' || ''  || '","' || image  || '","34ebcde5-91c4-4b6b-b98e-da7e7942e46c","' || guid  || '","' || st_astext(geom) || '");' from trails.trail_section;
-COMMENT ON VIEW trails.vw_trail_sections_sqlite_export IS 'This view generates insert statements for the sqlite database used in mobile apps.';
+COMMENT ON VIEW trails.vw_trail_sections_sqlite_export IS 'This view generates insert statements for the sqlite database used in mobile apps from the trail_section table.';
+
+
+create view trails.vw_trail_sections_m2m_sqlite_export as select 'insert into trail_sections (trail_guid, trail_section_guid, order) values ("' ||
+trail_guid  || '","' || trail_section_guid  || '", 0);' from (
+SELECT 
+  trail.guid as trail_guid, 
+  0, 
+  trail_section.guid as trail_section_guid
+FROM 
+  trails.trail_sections, 
+  trails.trail_section, 
+  trails.trail
+WHERE 
+  trail_sections.trail_id = trail.id AND
+  trail_sections.trail_section_id = trail_section.id
+) as trail_sections_guids;
+
+;
+COMMENT ON VIEW trails.vw_trail_sections_m2m_sqlite_export IS 'This view generates insert statements for the sqlite database used in mobile apps from the trail_sections table.';
+
+
 --
 -- TOC entry 3481 (class 2604 OID 165999)
 -- Name: category id; Type: DEFAULT; Schema: trails; Owner: timlinux
